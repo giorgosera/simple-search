cea5f3bca0c2b7b52588e8b9265a200a356dbd09
George Eracleous <basketballcy@gmail.com>
2014-09-21 04:56:43 +0300

    Started implementing indexer. For now we can tokenize and use stemming to find terms on a document

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
-var assert = require("assert");
+var assert = require("assert"),
+	indexer = require('../lib/indexer');
 
-describe('The crawler', function(){
-	it('should')
-});
-
-describe('Indexing', function(){
+describe('The indexer', function(){
+	it('should parse the text content of an HTML document', function(done){
+		var exampleHTML = '<html><head>This is the head</head><body><h1>This is the body</h1><div>Sample text</div></body></html>';
+		indexer.index('http://www.example.com', exampleHTML).then(function(){
+			done();
+		});
+    });
 });
 
 describe('Querying', function(){
